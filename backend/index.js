1cefd7660eccfe612c039be05e0b5cffffdb83d6
prajjwal <mishraprajjwal295@gmail.com>
2024-07-06 02:42:01 +0530

    //

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,11 @@ const express = require("express");
 const app = express();
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
-// const userRoutes = require("./routes/UserRoutes");
-const authRoutes = require("./routes/authRoutes");
 const cors = require("cors");
 
 const { cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
+const authRoutes = require("./routes/authRoutes");
 
 require("dotenv").config();
 const Port = process.env.PORT || 4000;
