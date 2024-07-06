1a4258b0212bf20cf7fc55fca1571fb6bbe0a685
prajjwal <mishraprajjwal295@gmail.com>
2024-07-06 22:47:08 +0530

    finall

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,30 +1,36 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
+import insta from "../assets/insta.png";
+import Image from "./Image";
 
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex justify-between items-center font-enter w-[100vw] mx-8  border-black-solid">
-      <div className=" flex-col gap-6 h-[100vh] items-center justify-between mx-auto text-[#d2e823] font-enter border-black-solid">
-        <div className="text-5xl font-extrabold my-10">
-          Welcome to Social View
-        </div>
+    <div className="flex max-md:flex-col">
+      <div className="flex flex-[50%] justify-between items-center font-enter w-[100vw] mx-8  border-black-solid">
+        <div className=" flex-col gap-6 h-[100vh] items-center justify-between mx-auto text-[#d2e823] font-enter border-black-solid">
+          <div className="text-5xl font-extrabold my-10">
+            Welcome to Social View
+          </div>
 
-        <div className="flex gap-3 max-md:flex-col justify-center items-center">
-          <button
-            className="rounded-md p-3 bg-[#eff0ec] text-xl font-semibold text-black"
-            onClick={() => navigate("/login")}
-          >
-            Log In
-          </button>
-          <button
-            className="rounded-md p-3 bg-black text-xl font-semibold text-white "
-            onClick={() => navigate("/signin")}
-          >
-            Sign Up Free
-          </button>
+          <div className="flex gap-3 max-md:flex-col justify-center items-center">
+            <button
+              className="rounded-md p-3 bg-[#eff0ec] text-xl font-semibold text-black"
+              onClick={() => navigate("/login")}
+            >
+              Log In
+            </button>
+            <button
+              className="rounded-md p-3 bg-black text-xl font-semibold text-white "
+              onClick={() => navigate("/signin")}
+            >
+              Sign Up Free
+            </button>
+          </div>
         </div>
+      </div>
+      <div className="flex flex-[50%] items-center justify-center w-full ">
+        <Image src={insta} alt="animated image" />
       </div>
     </div>
   );
---
+++ src/component/Image.js
@@ -0,0 +1,42 @@
+// src/components/Image.js
+import React, { useState } from "react";
+
+const Image = ({ src, alt }) => {
+  const [transform, setTransform] = useState({ rotateX: 0, rotateY: 0 });
+
+  const handleMouseMove = (event) => {
+    const { offsetWidth: width, offsetHeight: height } = event.currentTarget;
+    const { offsetX: x, offsetY: y } = event.nativeEvent;
+
+    const rotateY = (x / width - 0.5) * 60; 
+    const rotateX = -(y / height - 0.5) * 60; 
+
+    setTransform({
+      rotateX,
+      rotateY,
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setTransform({ rotateX: 0, rotateY: 0 });
+  };
+
+  return (
+    <div
+      className="w-1/2 h-1/2 overflow-hidden relative flex justify-center items-center mx-auto perspective-1000"
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+    >
+      <img
+        src={src}
+        alt={alt}
+        className="absolute transition-transform duration-100 ease-linear"
+        style={{
+          transform: `rotateX(${transform.rotateX}deg) rotateY(${transform.rotateY}deg)`,
+        }}
+      />
+    </div>
+  );
+};
+
+export default Image;
