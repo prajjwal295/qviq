a91ba13d805ea57ec25f1dc3ea2b3bf9657ea4df
prajjwal <mishraprajjwal295@gmail.com>
2024-07-06 09:24:24 +0530

    ..

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,6 +1,10 @@
 // firebaseAdmin.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json"); // Make sure to replace this with the actual path to your service account key JSON file
+require("dotenv").config();
+
+const serviceAccount = JSON.parse(process.env.serviceAccount);
+
+console.log(serviceAccount);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
